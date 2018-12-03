a384840cf7cf093547e0348eb5078dc1886c39f0
George-Vachnadze <45526363+George-Vachnadze@users.noreply.github.com>
2018-12-03 11:02:17 +0400

    Update onLoad.js

--- scripts/onLoad.js
+++ scripts/onLoad.js
@@ -1,3 +1,8 @@
 ﻿function onLoad() {
 	document.getElementById("footer").innerHTML = '<p class="center_element">Copyright © 2019 Website.ge</p>';
+	var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+	if (mobile) {
+		alert("Visit this on a Computer for Better View");              
+	} else {
+	}
 }
