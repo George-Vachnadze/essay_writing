7383326a987be266560e87882e18e5650de4df6e
George-Vachnadze <45526363+George-Vachnadze@users.noreply.github.com>
2018-12-03 11:04:44 +0400

    Update onLoad.js

--- scripts/onLoad.js
+++ scripts/onLoad.js
@@ -2,7 +2,7 @@
 	document.getElementById("footer").innerHTML = '<p class="center_element">Copyright © 2019 Website.ge</p>';
 	var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
 	if (mobile) {
-		alert("Visit this on a Computer for Better View");              
+		alert("ეწვიეთ ვებ-გვერდს კომპიუტერით! ზოგიერთი ვიზუალური ელემენტი მობილურისთვის მიუწვდომელია.");              
 	} else {
 	}
 }
